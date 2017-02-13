690d2ec9a57f4fd05e834b7211c0a7fb2aac780f
Aleks <aleks.stefanovic101@gmail.com>
2017-02-12 22:24:33 -0500

    commit to match heroku version

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 app.use(express.static('public'));
 
-app.listen(3000);
-
-console.log("Running at Port 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
